d73e6a84b152ef0c5fbadc23e4305ec00331dbd6
Luis Angel Iñiguez Hernández <luigi.lahi@gmail.com>
2017-10-23 15:10:28 -0500

    Model Route and BusStop modified to be manyToMany

---
+++ server/boot/create_admin_user.js
@@ -0,0 +1,31 @@
+'use strict';
+
+module.exports = (app) => {
+  const Profile = app.models.Profile;
+  const Role = app.models.Role;
+  Profile.count({
+    where: {username: 'admin'},
+  }, (err, count) => {
+    if (count < 1) {
+      return;
+    }
+    Profile.create({
+      username: 'admin', password: '11223344', email: 'luigilahi@gmail.com',
+    }, (err, profile) => {
+      if (err) throw err;
+      console.log('Admin user created');
+      Role.create({
+        name: 'admin',
+      }, (err, role) =>  {
+        if (err) throw err;
+        console.log('Role created');
+        role.principals.create({
+          principalType: 'Profile',
+          principalId: profile.id,
+        }, (err, principal) => {
+          if (err) throw err;
+        });
+      });
+    });
+  });
+};
